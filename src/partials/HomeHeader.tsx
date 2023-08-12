795ffce51babf16368c8ae4f225e2d676e4a4772
rizveejack <jrizvee@gmail.com>
2023-08-12 12:30:25 +0600

    fix staf

--- src/partials/HomeHeader.tsx
+++ src/partials/HomeHeader.tsx
@@ -12,7 +12,7 @@ export default function HomeHeader() {
             >
                 <source src="/images/bg.mp4" type="video/mp4" />
             </video>
-            <div className="absolute min-h-full min-w-full bg-black/75 top-0 left-0 flex flex-col justify-center items-center p-5 pb-28 lg:pb-0">
+            <div className="absolute min-h-full min-w-full bg-black/75 top-0 left-0 flex flex-col justify-center items-center p-5 pb-28">
                 <Image
                     src={'/images/logo.png'}
                     alt="Logo"
@@ -34,15 +34,15 @@ export default function HomeHeader() {
                     Enroll Now
                 </Link>
             </div>
-            <div className="absolute h-44 w-full lg:bottom-10 left-0 bottom-0">
+            <div className="absolute h-44 w-full lg:bottom-5 left-0 bottom-0">
                 <div className="max-w-7xl mx-auto bg-black/20 grid-cols-3 grid text-center text-white h-full items-center rounded-md">
                     <div>
                         <h3 className="text-6xl ">10</h3>
                         <h3>GLOBAL LOCATIONS</h3>
                     </div>
                     <div>
-                        <h3 className="text-6xl">10+</h3>
-                        <h3>PROGRAMS & COURSES</h3>
+                        <h3 className="text-6xl">06+</h3>
+                        <h3>UNIVERSITY</h3>
                     </div>
                     <div>
                         <h3 className="text-6xl">25</h3>
--- src/widgets/Associated.tsx
+++ src/widgets/Associated.tsx
@@ -1,67 +1,32 @@
-import Image from "next/image";
+import Image from 'next/image'
 export default function Associated() {
-  return (
-    <>
-      <div className="max-w-2xl mx-auto text-center lg:py-20 py-10">
-        <strong className="text-3xl font-bold">
-          University We already Associated With
-        </strong>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis,
-          iusto quis. Quibusdam dolorem neque perspiciatis sunt vitae dolor
-          commodi ipsam.
-        </p>
-      </div>
-      <div className="grid grid-cols-2 lg:grid-cols-6 grid-flow-row max-w-7xl mx-auto items-center justify-center mb-20 gap-10 p-5">
-        <div className="rounded-s-full aspect-square relative">
-          <Image
-            alt="logo"
-            src={"/images/kyoto.png"}
-            fill
-            className="object-cover "
-          />
-        </div>
-        <div className="rounded-s-full aspect-square relative">
-          <Image
-            alt="logo"
-            src={"/images/sig.png"}
-            fill
-            className="object-cover "
-          />
-        </div>
-        <div className="rounded-s-full aspect-square relative">
-          <Image
-            alt="logo"
-            src={"/images/umm.png"}
-            fill
-            className="object-cover "
-          />
-        </div>
-        <div className="rounded-s-full aspect-square relative">
-          <Image
-            alt="logo"
-            src={"/images/fcub.png"}
-            fill
-            className="object-cover "
-          />
-        </div>
-        <div className="rounded-s-full aspect-square relative">
-          <Image
-            alt="logo"
-            src={"/images/usu.png"}
-            fill
-            className="object-cover "
-          />
-        </div>
-        <div className="rounded-s-full aspect-square relative">
-          <Image
-            alt="logo"
-            src={"/images/ver.png"}
-            fill
-            className="object-cover "
-          />
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="max-w-2xl mx-auto text-center lg:py-20 py-10">
+                <strong className="text-3xl font-bold">
+                    University We already Associated With
+                </strong>
+            </div>
+            <div className="grid grid-cols-2 lg:grid-cols-6 grid-flow-row max-w-7xl mx-auto items-center justify-center mb-20 gap-10 p-5">
+                <div className="rounded-s-full aspect-square relative">
+                    <Image alt="logo" src={'/images/kyoto.png'} fill className="object-cover " />
+                </div>
+                <div className="rounded-s-full aspect-square relative">
+                    <Image alt="logo" src={'/images/sig.png'} fill className="object-cover " />
+                </div>
+                <div className="rounded-s-full aspect-square relative">
+                    <Image alt="logo" src={'/images/umm.png'} fill className="object-cover " />
+                </div>
+                <div className="rounded-s-full aspect-square relative">
+                    <Image alt="logo" src={'/images/fcub.png'} fill className="object-cover " />
+                </div>
+                <div className="rounded-s-full aspect-square relative">
+                    <Image alt="logo" src={'/images/usu.png'} fill className="object-cover " />
+                </div>
+                <div className="rounded-s-full aspect-square relative">
+                    <Image alt="logo" src={'/images/ver.png'} fill className="object-cover " />
+                </div>
+            </div>
+        </>
+    )
 }
